b33833b9a74cfb62f0c551ea72d6d7c9caa62ae5
Jakub Olszanecki <olszanecki11@gmail.com>
2023-11-05 17:09:25 +0100

    update seeder/seeder

--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -9,22 +9,37 @@ const User = require('../models/UserModel');
 const Class = require('../models/ClassModel');
 
 const importData = async () => {
-  try{
-    await User.collection.deleteMany({});
-    await Class.collection.deleteMany({});
+  try {
+    if (process.argv[2] === "-a") { //all
+      await User.collection.deleteMany({});
+      await Class.collection.deleteMany({});
 
-    if(process.argv[2] !== "-d"){
       await User.insertMany(userData);
       await Class.insertMany(classData);
 
       console.log("Seeder data imported SUCCESSFULLY!");
       process.exit();
-      return;
+
+    }
+    else if(process.argv[2] === "-d"){ //delete all
+
+      await User.collection.deleteMany({});
+      await Class.collection.deleteMany({});
+
+      console.log("Seeder data DELETED SUCCESSFULLY!");
+      process.exit();
+    }
+    else{
+      await Class.collection.deleteMany({});
+
+      await Class.insertMany(classData);
+
+      console.log("Seeder data for classes imported SUCCESSFULLY!");
+      process.exit();
+
     }
 
-    console.log("Seeder data DELETED successfully!");
-    process.exit();
-  }catch(error){
+  } catch (error) {
     console.log("Error while processing seeder data", error);
     process.exit(1);
   }
